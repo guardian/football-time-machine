0562a58b4d38a63036ea7ca2d52b7a4fc5adf771
Adam Fisher <adam.fisher@guardian.co.uk>
2014-02-26 15:35:53 +0000

    Improve homepage info

--- app/controllers/TimeMachine.scala
+++ app/controllers/TimeMachine.scala
@@ -8,14 +8,14 @@ import pa.FootballClient
 
 object TimeMachine extends Controller with Slugs with FootballClient with LocalDisk with ExecutionContexts {
 
-  def index = Action {
+  def index = Action { implicit request =>
 
     val dateTimes = for {
       date <- listFiles("")
       time <- listFiles(date)
     } yield (date, time)
 
-    Ok(views.html.index(dateTimes))
+    Ok(views.html.index(dateTimes.groupBy(_._1).toList.sortBy(_._1)))
   }
 
 }
