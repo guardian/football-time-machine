d9a326fbc9c3c5e42c78bbb0483ca2ac75d188f1
Andrew Bulhak <andrew.bulhak@theguardian.com>
2014-04-23 11:35:53 +0100

    Adding Plugins.scala

---
+++ project/project/Plugins.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+object Plugins extends Build {
+  val playArtifactPluginVersion = "play2.2.0_3"
+
+  lazy val plugins = Project("main", file(""))
+    .dependsOn(uri("git://github.com/guardian/sbt-play-artifact.git#" + playArtifactPluginVersion))
+}
