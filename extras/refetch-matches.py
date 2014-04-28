150a1d373fb4e3f805e7597a03f11a470a65de8b
Andrew Bulhak <andrew.bulhak@theguardian.com>
2014-04-28 18:18:59 +0100

    Modified refetch-matches to pay attention to matchDay

--- extras/refetch-matches.py
+++ extras/refetch-matches.py
@@ -4,20 +4,31 @@
 Force the PA football proxy to refetch a snapshot of today's matches.
 """
 import os
+import time
 import urllib
 import xml.etree.ElementTree as ET
 
 PROXY_URL="http://54.72.218.177:9000/record"
 PA_API_KEY=os.environ.get('PA_API_KEY')
 
-# fetch the fixtures
+matches = set()
+
+# fetch the fixtures/match day URLs
+
+matchdayurl = '%s/api/football/competitions/matchDay/%s/%s'%(PROXY_URL,PA_API_KEY,time.strftime("%Y%m%d"))
+tree = ET.parse(urllib.urlopen(matchdayurl))
+for elem in tree.iter('match'):
+	matches.add(elem.get('matchID'))
 
 for fixture in ['100', '500']:
 	url = '%s/api/football/competition/fixtures/%s/%s'%(PROXY_URL, PA_API_KEY, fixture)
 	tree = ET.parse(urllib.urlopen(url))
-	matches = [c.get('matchID') for c in tree.iter('fixture')]
-	for match in matches:
-		for urltype in ['events', 'lineups']:
-			matchurl = '%s/api/football/match/%s/%s/%s'%(PROXY_URL,urltype,PA_API_KEY,match)
-			urllib.urlopen(matchurl).read()
+	for elem in tree.iter('fixture'):
+		matches.add(elem.get('matchID'))
+
+print "matches = %r"%matches
+for match in matches:
+	for urltype in ['events', 'lineups']:
+		matchurl = '%s/api/football/match/%s/%s/%s'%(PROXY_URL,urltype,PA_API_KEY,match)
+		urllib.urlopen(matchurl).read()
 
