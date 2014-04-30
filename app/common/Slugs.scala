a46d3c3cbda129d3ed76f5472e1c7a1f683f62a0
Andrew Bulhak <andrew.bulhak@theguardian.com>
2014-04-30 11:44:11 +0100

    Fixed bug in date regex which failed after 29th of each month

--- app/common/Slugs.scala
+++ app/common/Slugs.scala
@@ -3,7 +3,7 @@ package common
 import conf.Configuration
 
 trait Slugs {
-  val DateSlugRegex = """(201\d[01]\d[012]\d)""".r
+  val DateSlugRegex = """(201\d[01]\d[0123]\d)""".r
   val TimeSlugRegex = """([0-2][0-9][0-5][0-9])""".r
 
   def slugsToFilePath(slugs: List[String]): String = {
