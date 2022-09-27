6f3c019b89a10ea2fa8e30efc0c9248ace9e5d10
Francesca Hammond <francesca.hammond@guardian.co.uk>
2022-09-27 17:26:50 +0100

    Add logging

--- archive/src/main/scala/com/gu/footballtimemachine/Configuration.scala
+++ archive/src/main/scala/com/gu/footballtimemachine/Configuration.scala
@@ -7,21 +7,21 @@ import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.gu.conf.{ConfigurationLoader, SSMConfigurationLocation}
 import com.gu.{AppIdentity, AwsIdentity}
 import com.typesafe.config.Config
+import org.slf4j.LoggerFactory
 import software.amazon.awssdk.auth.credentials.{AwsCredentialsProviderChain => AwsCredentialsProviderChainV2, DefaultCredentialsProvider => DefaultCredentialsProviderV2, ProfileCredentialsProvider => ProfileCredentialsProviderV2}
 
 import scala.util.{Failure, Success, Try}
 
 class Configuration {
+  val logger = LoggerFactory.getLogger(this.getClass)
 
   val credentials = new AWSCredentialsProviderChain(
     new ProfileCredentialsProvider("mobile"),
-    DefaultAWSCredentialsProviderChain.getInstance()
-  )
+    DefaultAWSCredentialsProviderChain.getInstance())
 
   val credentialsv2 = AwsCredentialsProviderChainV2.of(
     ProfileCredentialsProviderV2.builder.profileName("mobile").build,
-    DefaultCredentialsProviderV2.create
-  )
+    DefaultCredentialsProviderV2.create)
 
   val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard()
     .withCredentials(credentials)
@@ -38,12 +38,18 @@ class Configuration {
       config <- Try(ConfigurationLoader.load(identity, credentialsv2) {
         case AwsIdentity(app, stack, stage, _) =>
           SSMConfigurationLocation(path = s"/$app/$stage/$stack", region = Regions.EU_WEST_1.toString)
-        })
+      })
     } yield config
 
   val config: Config = conf match {
-    case Success(config) => config
-    case Failure(err) => throw err
+    case Success(config) => {
+      logger.info("Successfully loaded configuration")
+      config
+    }
+    case Failure(err) => {
+      logger.info("Failed to load configuration")
+      throw err
+    }
   }
 
   val paApiKey: String = config.getString("pa.api-key")
