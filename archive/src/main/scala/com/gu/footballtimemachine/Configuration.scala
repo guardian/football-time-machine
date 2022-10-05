37ad7d9a8e17db1324f14f0d1792166774d45c83
David Lawes <david.lawes@guardian.co.uk>
2022-10-05 16:45:46 +0100

    tidy up logging

--- archive/src/main/scala/com/gu/footballtimemachine/Configuration.scala
+++ archive/src/main/scala/com/gu/footballtimemachine/Configuration.scala
@@ -1,6 +1,6 @@
 package com.gu.footballtimemachine
 
-import com.amazonaws.auth.{AWSCredentialsProviderChain, DefaultAWSCredentialsProviderChain, EnvironmentVariableCredentialsProvider, InstanceProfileCredentialsProvider}
+import com.amazonaws.auth.{AWSCredentialsProviderChain, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
@@ -8,21 +8,15 @@ import com.gu.{AppIdentity, AwsIdentity, DevIdentity}
 import com.gu.conf.{ConfigurationLoader, SSMConfigurationLocation}
 import com.typesafe.config.Config
 import software.amazon.awssdk.auth.credentials.{AwsCredentialsProviderChain => AwsCredentialsProviderChainV2, DefaultCredentialsProvider => DefaultCredentialsProviderV2, ProfileCredentialsProvider => ProfileCredentialsProviderV2}
-import org.slf4j.{Logger, LoggerFactory}
 
 class Configuration {
-  private val configurationLogger: Logger = LoggerFactory.getLogger("ConfigurationLogger")
-  configurationLogger.info("About to get credentials")
-
   val credentials = new AWSCredentialsProviderChain(
     new ProfileCredentialsProvider("mobile"),
     DefaultAWSCredentialsProviderChain.getInstance())
 
   val credentialsv2: AwsCredentialsProviderChainV2 = AwsCredentialsProviderChainV2.of(
-//    ProfileCredentialsProviderV2.builder.profileName("mobile").build,
+    ProfileCredentialsProviderV2.builder.profileName("mobile").build,
     DefaultCredentialsProviderV2.create)
-
-  configurationLogger.info("Got credentials")
 
   val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard()
     .withCredentials(credentials)
@@ -32,8 +26,6 @@ class Configuration {
   val stack: String = Option(System.getenv("Stack")).getOrElse("mobile")
   val stage: String = Option(System.getenv("Stage")).getOrElse("CODE")
   val app: String = Option(System.getenv("App")).getOrElse("football-time-machine")
-
-  configurationLogger.info("About to get configuration")
 
   val conf: Config = {
     val identity = AppIdentity.whoAmI(defaultAppName = app)
@@ -45,11 +37,7 @@ class Configuration {
     }
   }
 
-  configurationLogger.info("Got configuration")
-
   val paApiKey: String = conf.getString("pa.api-key")
   val paHost: String = conf.getString("pa.host")
-
-  configurationLogger.info("Got ssm values")
 
 }
